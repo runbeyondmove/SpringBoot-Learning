3427eaf27ac776ab893d7fc1a58105a5bbbe0d46
runbeyondmove <runbeyondmove@163.com>
2018-01-23 17:46:34 +0800

    Spring Boot中使用AOP统一处理Web请求日志-测试类修改，SpringApplicationConfiguration注解过时

--- Chapter4-2-4/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter4-2-4/src/test/java/com/didispace/ApplicationTests.java
@@ -3,12 +3,13 @@ package com.didispace;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest
 public class ApplicationTests {
 
 	private Logger logger = Logger.getLogger(getClass());
